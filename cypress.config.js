9a3f862581526a17d30a0f08c459b8c916e29ada
pacomendieta <ff.mendieta@gmail.com>
2023-03-14 13:18:27 +0100

    Probando errores eslint

--- cypress.config.js
+++ cypress.config.js
@@ -1,8 +1,8 @@
 module.exports = {
   video: false,
   e2e: {
-    setupNodeEvents(on, config) {},
-    supportFile:false, // Evitar error si no existe carpeta /support   
+    setupNodeEvents(_on, _config) {},
+    supportFile:false,// Evitar error si no existe carpeta /support
   },
   chromeWebSecurity:false,   // Para https en Chrome
 }
