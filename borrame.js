82d7f1a55650750da62b0e95e01affeeeab16ab6
pacomendieta <ff.mendieta@gmail.com>
2023-03-14 13:20:43 +0100

    Probando eslint

---
+++ borrame.js
@@ -0,0 +1,27 @@
+// development.env
+NODE_ENV=development
+HOST=127.0.0.1
+PORT=5000
+
+
+// production.env
+NODE_ENV=production
+HOST=localhost
+PORT=8000
+
+
+// config.js
+const dotenv = require('dotenv');
+const path = require('path');
+
+// configurar a dotenv el fichero production.env o development.env:
+dotenv.config({
+  path: path.resolve(__dirname, process.env.NODE_ENV + '.env')
+});
+
+// las variables de entorno definidas en el archivo *.env ahora están en process.env
+module.exports = {
+  NODE_ENV: process.env.NODE_ENV || 'development',
+  HOST: process.env.HOST || '127.0.0.1',
+  PORT: process.env.PORT || 3000
+}
--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   video: false,
   e2e: {
-    setupNodeEvents(_on, _config) {},
+    setupNodeEvents(/*_on, _config*/) {},
     supportFile:false,// Evitar error si no existe carpeta /support
   },
   chromeWebSecurity:false,   // Para https en Chrome
